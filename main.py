7529ba97cc2f0cd332426cbb6e459cf5319e0eee
Ernesto Brau Monteverde <noodle.netis@gmail.com>
2025-11-24 21:11:03 -0500

    fixed main

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import sys
-import csv
-import pandas as pd
 
-from util.contact import Contact
-from util.structures import mergesort
-from util.structures import ContactBST
+import pandas as pd
 
 
 def main():
-
     file = sys.argv[1]
     csv_file = pd.read_csv(file)
     sadfewfsef = csv_file['first_name']
@@ -18,12 +13,10 @@ def main():
         balls.append(row)
 
     sorted_list = sorted(balls)
-    #print(sorted_list)
+    # print(sorted_list)
 
     sorted_phones = sorted(isudfoisaufd)
     print(sorted_phones)
-
-
 
 
 if __name__ == "__main__":
